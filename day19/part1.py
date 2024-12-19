2806a28733d02b7762eb92c4625b7126761f3efd
Gaston Fartek <gastonfartek@gmail.com>
2024-12-19 06:41:36 -0500

    day19

---
+++ day19/part1.py
@@ -0,0 +1,46 @@
+def solve(input_str: str):
+
+  a, b = input_str.split('\n\n')
+
+  towels = set(a.split(", "))
+
+  cache = {}
+  def count(s: str):
+    if s in cache:
+     return cache[s]
+
+    if len(s) == 0:
+      return 1
+
+    result = 0
+    for t in towels:
+      if s.startswith(t):
+        result += count(s[len(t):])
+    cache[s] = result
+    return result
+
+  ans = 0
+  for design in b.splitlines():
+    ans += 1 if count(design) > 0 else 0
+
+
+  return ans
+
+
+SAMPLE_INPUT = """r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"""
+
+print("sample input: ", solve(SAMPLE_INPUT, 6, 12))
+
+with open("day19/input.txt", "r") as input_file:
+    print("input: ", solve(input_file.read(), 70, 1024))
+
+
